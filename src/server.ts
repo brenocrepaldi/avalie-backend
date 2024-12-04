634dfe7f449883409e757d81a29b030b5620c2c4
brenocrepaldi <brenogaia2004@gmail.com>
2024-12-04 00:15:07 -0300

    refactor: changes route handling

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
 import express from 'express';
-import routes from './routes';
-import { env } from './env';
+
 import { connectDB } from './db';
+import { env } from './env';
+
+import courseRoutes from './routes/course-routes';
+import directorRoutes from './routes/director-routes';
+import disciplineRoutes from './routes/discipline-routes';
+import feedbackRoutes from './routes/feedback-routes';
+import professorRoutes from './routes/professor-routes';
+import studentRoutes from './routes/student-routes';
 
 const app = express();
 
 // Middleware to use routes
 app.use(express.json());
-app.use(routes);
+
+// Routes
+app.use('/course', courseRoutes);
+app.use('/director', directorRoutes);
+app.use('/discipline', disciplineRoutes);
+app.use('/feedback', feedbackRoutes);
+app.use('/professor', professorRoutes);
+app.use('/student', studentRoutes);
 
 // Database connection
 connectDB()
